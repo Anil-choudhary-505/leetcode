693aadcb7dfaef882da7b5bac8fd5998c88a23c7
Anil-choudhary-505 <anilchoudhary92004@gmail.com>
2024-10-20 11:48:21 +0530

    first commit

---
+++ stack/asteriod collison.cpp
@@ -0,0 +1,43 @@
+// 735 - Asteriod collision
+
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        for(int &a : asteroids){
+
+            while(!st.empty() && a<0 && st.top()>0){
+                int sum = a + st.top();
+
+                if(sum < 0){
+                    st.pop();
+                }else if(sum > 0){
+                    a=0;
+                }else{
+                    st.pop();
+                    a=0;
+                }
+            }
+            
+            if(a!=0){
+                st.push(a);
+            }
+        }
+    int n=st.size();
+        vector<int>result(n);
+    
+    int i=n-1;
+    while(!st.empty()){
+        result[i]=st.top();
+        st.pop();
+        i--;
+    }
+    return result;
+    }
+};
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<stack>
